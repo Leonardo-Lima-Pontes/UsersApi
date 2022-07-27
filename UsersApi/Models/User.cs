e983a543e829d845c1c331cd139a8aaed1e5f10d
Leonardo-Lima-Pontes <bruxodeveloper@hotmail.com>
2022-07-27 12:02:17 -0300

    criação(Models): Model para usuário criada.

---
+++ UsersApi/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace UsersApi.Models;
+
+public class User
+{
+    [Key]
+    public int UserId { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    [MinLength(3)]
+    [DisplayName("Primeiro Nome")]
+    public string FirstName { get; set; } = default!;
+
+    [Required]
+    [MaxLength(100)]
+    [MinLength(3)]
+    [DisplayName("Sobrenome")]
+    public string LastName { get; set; } = default!;
+}
