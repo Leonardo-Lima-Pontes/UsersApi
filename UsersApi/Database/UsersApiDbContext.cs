1b608b9568c4695a8875cf49a4eaaeed9a3312e9
Leonardo-Lima-Pontes <bruxodeveloper@hotmail.com>
2022-07-27 12:12:49 -0300

    creation(DbContext): DbContext para UsersApi project criada.

---
+++ UsersApi/Database/UsersApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersApi.Models;
+
+namespace UsersApi.Database;
+
+public class UsersApiDbContext : DbContext
+{
+    public UsersApiDbContext(DbContextOptions<UsersApiDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<User>? Users { get; set; }
+}
